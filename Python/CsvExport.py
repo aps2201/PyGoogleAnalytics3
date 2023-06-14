098412f20770d8242d445e603380698ff934d11e
aps2201 <6870663+aps2201@users.noreply.github.com>
2023-06-15 00:24:44 +0800

    renamed CsvExport to Export for future-proofing

--- Python/CsvExport.py
+++
@@ -1,7 +0,0 @@
-import csv
-
-def csv_export(filename:str,report:list,header:list):
-    with open(filename, 'w') as csvfile:
-        writer = csv.DictWriter(csvfile, fieldnames = header)
-        writer.writeheader()
-        writer.writerows(report)
